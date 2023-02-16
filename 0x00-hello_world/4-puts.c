10d7aadc22a60620016fa108d3dd95abf1a3dfd1
subinked <122836157+SubInked@users.noreply.github.com>
2023-02-16 13:50:11 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - use the pus function to print a statsment
+ * Return: 0 (code was Successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
