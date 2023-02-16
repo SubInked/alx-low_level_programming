cc4f1cada2e81e9335a0c7fa7a9f12028facccb5
subinked <122836157+SubInked@users.noreply.github.com>
2023-02-16 14:02:35 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - write printf function that prints exact line
+* Return: 0 (code was successful)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
