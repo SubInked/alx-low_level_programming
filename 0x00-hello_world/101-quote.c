c04e6695e56efc71a0d8b9e0f8c48f6f9eda28a8
subinked <122836157+SubInked@users.noreply.github.com>
2023-02-16 15:58:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - a programme that prints an exact line ending with a new line
  * Return: 1 (code is successful)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 return (1);
 }
