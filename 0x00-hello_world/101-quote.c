7e72e3f62f2251a749c6121fecacfd94e727dc0b
subinked <122836157+SubInked@users.noreply.github.com>
2023-02-16 15:46:29 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a programme that prints an exact line ending with a new line
+ * Return: 1 (code is successful)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
