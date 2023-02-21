45ad4b751d966d9fb4615d50a0b8b45508ee331c
subinked <122836157+SubInked@users.noreply.github.com>
2023-02-21 15:38:59 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,26 @@
 #include "main.h"
 /**
- * print_alphabet - print all alphabets in lower case
+ * print_alphabet_x10 - print alphabets in lower case x10
  */
-
-
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char letter;
-		for(letter = 'a'; letter<='z'; letter++);
-		_putchar(letter);
-		putchar(\n);
+	char ch;
+	int i;
+
+
+	i = 0;
+
+
+	while (i <= 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+
+	_putchar ('\n');
+	i++;
+	}
 }
