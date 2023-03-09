2b7d6494899d0d1945060ec6795a595b620af008
subinked <122836157+SubInked@users.noreply.github.com>
2023-03-08 19:00:38 -0800

    6-prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int rec_prime(int n, int i);
+
+/**
+ * is_prime_number - indicates if integer is a prime number or not
+ * @n: input number
+ *
+ * Return: 1 if n is a prime number, 0 if not
+ */
+
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (rec_prime(n, n - 1));
+}
+
+/**
+ * rec_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+
+
+int rec_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (rec_prime(n, i - 1));
+}
