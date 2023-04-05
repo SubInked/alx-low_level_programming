ab3196ce9c3dcefefc49ab2e1f51c13f7c5b10b7
subinked <122836157+SubInked@users.noreply.github.com>
2023-04-05 14:44:09 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list and sets
+ * head to NULL.
+ *
+ * @head: A pinter to the head of list.
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+
+	if (head == NULL)
+		return;
+
+	if (head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
