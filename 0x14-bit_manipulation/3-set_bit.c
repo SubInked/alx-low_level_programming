e2b58e736248495ee69c9192ce7da0d3486dbe8d
SubInked <subliminally.inked@gmail.com>
2023-07-06 11:33:40 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,11 +11,7 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int i;
-
-	(int i = (sizeof(unsigned long int) * 8) - 1);
-
-	if (index > i)
+	if (index > (sizeof(unsigned long int) * 8) - 1)
 		return (-1);
 
 	*n |= (1 << index);
